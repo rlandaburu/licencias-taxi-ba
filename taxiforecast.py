359fe79c757fce39a55486ccb4572a1dba22266c
rlandaburu <52452298+rlandaburu@users.noreply.github.com>
2021-01-21 19:48:17 -0300

    Update taxiforecast.py

--- taxiforecast.py
+++ taxiforecast.py
@@ -12,7 +12,9 @@ data['ds'] = pd.to_datetime(data['ds'],errors='coerce')
 
 max_date = data['ds'].max()
 
-periods_input = 12
+st.sidebar.header('Elegi cuantos meses a proyectar')
+periods_input = st.sidebar.selectbox("Meses", (12,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24))
+
 m = Prophet(seasonality_mode='multiplicative')
 m.fit(data)
  
